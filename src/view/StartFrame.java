511f5f4fed2c7b0f5d88143d78d6cc6da04bca7b
jeff-anna <12213035@mail.sustech.edu.cn>
2023-05-16 20:09:20 +0800

    添加了开始界面

---
+++ src/view/StartFrame.java
@@ -0,0 +1,39 @@
+package view;
+
+import controller.GameController;
+import model.Chessboard;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class StartFrame extends JFrame{
+    private final int WIDTH;
+    private final int HEIGTH;
+
+    private final int ONE_CHESS_SIZE;
+
+    private ChessboardComponent chessboardComponent;
+    private GameController gameController;
+    public StartFrame(int width, int height) {
+        setTitle("斗兽棋"); //设置标题
+        this.WIDTH = width;
+        this.HEIGTH = height;
+        this.ONE_CHESS_SIZE = (HEIGTH * 4 / 5) / 9;
+
+        setSize(WIDTH, HEIGTH);
+        setLocationRelativeTo(null); // Center the window.
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //设置程序关闭按键，如果点击右上方的叉就游戏全部关闭了
+        setLayout(new BorderLayout());
+        addBeginButton();
+    }
+    private void addBeginButton() {
+        JButton button = new JButton("Begin");
+        button.addActionListener((e) -> {
+            ChessGameFrame mainFrame = new ChessGameFrame(1100, 810);
+            GameController gameController = new GameController(mainFrame.getChessboardComponent(), new Chessboard());
+            mainFrame.setVisible(true);
+        });
+        button.setSize(240,80);
+        add(button);
+    }
+}
