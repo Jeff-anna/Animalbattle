e7abe0911f1546b58c957a866ab3b80ca12132cd
jeff-anna <12213035@mail.sustech.edu.cn>
2023-05-20 15:23:32 +0800

    解决了Start界面不显示的问题

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,15 @@
 import controller.GameController;
 import model.Chessboard;
 import view.ChessGameFrame;
+import view.StartFrame;
+
 import javax.swing.*;
 import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame start=new JFrame();
-            start.setSize(1100,810);
-            start.setVisible(true);
-            start.setLocationRelativeTo(null);
-            JPanel jp=new JPanel(new FlowLayout());
-            JButton begin=new JButton("Begin");
-            begin.setSize(300,100);
-            begin.setVisible(true);
-            jp.add(begin);
-            start.add(jp);
-            start.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-            ChessGameFrame mainFrame = new ChessGameFrame(1100, 810);
-            GameController gameController = new GameController(mainFrame.getChessboardComponent(), new Chessboard());
-            mainFrame.setVisible(true);
+            StartFrame sf=new StartFrame(700,500);
+            sf.setVisible(true);
         });
     }
 }
